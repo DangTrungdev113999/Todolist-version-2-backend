59e5aa9e89bd51611c56a1a6270cf458cab00b56
dangtrungdev <dangtrungdev113999@gmail.com>
2019-12-30 08:54:21 +0700

    fix CORS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import connectDB from './config/connectDB';
 
 import taskRouter from './routes/task.router';
@@ -19,14 +21,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'POST, GET, PUT, PATCH, DELETE, OPTIONS',
+//   );
+//   next();
+// });
 
 app.use('/api/v1/task', taskRouter);
 
