fba2684b8cf9015c2b37dd9a6b07445c5d5b3035
dangtrungdev <dangtrungdev113999@gmail.com>
2019-12-30 08:43:53 +0700

    CROS

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 app.use('/api/v1/task', taskRouter);
 
 module.exports = app;
